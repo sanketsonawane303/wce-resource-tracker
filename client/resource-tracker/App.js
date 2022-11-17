b4f0ea6a5d9761ccc99064bd2a8942b596ce580c
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-11-17 21:40:04 +0530

    Update styles

--- client/resource-tracker/App.js
+++ client/resource-tracker/App.js
@@ -26,13 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    marginTop: statusbar
-    // alignItems: 'center',
-    // justifyContent: 'center',
-=======
     backgroundColor: '#fff',
     marginTop: statusbar,
->>>>>>> 3d26e1f93d2cf594f72360d5a24704e94afdd575
   },
 });
