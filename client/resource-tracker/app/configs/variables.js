943fa7c23750cab20f73cd700233121afb9c924b
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-11-22 13:11:32 +0530

    Add status colors

--- client/resource-tracker/app/configs/variables.js
+++ client/resource-tracker/app/configs/variables.js
@@ -9,5 +9,9 @@ const colors = {
   lightskyblue: "#87CEFA",
   lightgrey: "#D3D3D3",
   lightblue: "#e6ecff",
+  successbg: "#ccffcc",
+  pendingbg: "#ffe0b3",
+  rejectbg: "#ffb3b3"
+
 };
 export { statusbar, colors };
