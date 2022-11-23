8683bb2035388b265266988b99e023a2c28369d9
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2022-11-23 11:57:07 +0530

    Add QR modal on viewRequest screen

--- client/resource-tracker/app/screens/ViewRequest.js
+++ client/resource-tracker/app/screens/ViewRequest.js
@@ -1,10 +1,7 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Modal, Image } from "react-native";
 import React, { useState } from "react";
 import AppButton from "../components/AppButton";
-import { StatusBar, Modal, Pressable } from "react-native";
 import ShowTitleInfo from "../components/ShowTitleInfo";
-import RNEInput from "../components/RNEInput";
-import { Formik } from "formik";
 import { Input } from "react-native-elements";
 import { colors } from "../configs/variables";
 
@@ -19,11 +16,12 @@ const obj = [
 
 export default function ViewRequest() {
   const [role, setRole] = useState("teacher");
-  const [modalVisible, setModalVisible] = useState(false);
+  const [suggestModalVisible, setSuggestModalVisible] = useState(false);
   const [suggestions, setSuggestions] = useState("");
+  const [qrModalVisible, setQRModalVisible] = useState(false);
 
   const handleSuggestionSubmit = (values) => {
-    setModalVisible(!modalVisible);
+    setSuggestModalVisible(!suggestModalVisible);
     console.log(suggestions);
   };
   return (
@@ -39,11 +37,11 @@ export default function ViewRequest() {
           })}
         </View>
 
-        {role == "teacher" ? (
+        {role !== "teacher" ? (
           <>
             <View style={styles.buttonGroup}>
               <AppButton
-                onPress={() => setModalVisible(!modalVisible)}
+                onPress={() => setSuggestModalVisible(!modalVisible)}
                 title={"Ask Details"}
               />
               <AppButton buttonStyles={styles.button} title={"Reject"} />
@@ -54,6 +52,7 @@ export default function ViewRequest() {
           <>
             <View style={styles.buttonGroup}>
               <AppButton buttonStyles={styles.button} title={"Edit"} />
+              <AppButton onPress={() => setQRModalVisible(!qrModalVisible)} title={"Show QR"} />
               <AppButton title={"Withdraw"} />
             </View>
           </>
@@ -65,9 +64,9 @@ export default function ViewRequest() {
         <Modal
           animationType="slide"//slide, fade, none
           transparent={true}
-          visible={modalVisible}
+          visible={suggestModalVisible}
           onRequestClose={() => {
-            setModalVisible(!modalVisible);
+            setSuggestModalVisible(!suggestModalVisible);
           }}
          
         >
@@ -97,6 +96,32 @@ export default function ViewRequest() {
                 onPress={handleSuggestionSubmit}
                 buttonStyles={{ padding: 12 }}
                 title={"Submit"}
+              />
+            </View>
+          </View>
+        </Modal>
+      </View>
+
+      <View>
+        <Modal
+          animationType="slide"//slide, fade, none
+          transparent={true}
+          visible={qrModalVisible}
+          onRequestClose={() => {
+            setQRModalVisible(!qrModalVisible)
+          }}
+         
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+
+              <Image style={styles.image} source={require("../../assets/testQR.png")} ></Image>
+             
+              <AppButton
+                
+                buttonStyles={{ padding: 12 }}
+                title={"Hide QR"}
+                onPress={() => setQRModalVisible(!qrModalVisible)}
               />
             </View>
           </View>
@@ -156,4 +181,8 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: "center",
   },
+  image:{
+    width: 300, 
+    height:300
+  }
 });
