9fd587a5f2160fe2ff19691f30abd8acce7150f3
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2022-11-27 14:58:40 +0530

    bug fix

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import getResources from "../controllers/resource/getResources";
-import createResource from "../controllers/resource/createResource";
+import getResources from "../controllers/resource/getResources.js";
+import createResource from "../controllers/resource/createResource.js";
 
 import role from "../middlewares/role.js";
 
