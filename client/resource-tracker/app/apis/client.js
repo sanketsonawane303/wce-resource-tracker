dc603061343ed2b07f0da401d49392994d93ea41
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-11-28 22:57:54 +0530

    Update url

--- client/resource-tracker/app/apis/client.js
+++ client/resource-tracker/app/apis/client.js
@@ -1,6 +1,6 @@
 import { create } from "apisauce";
 const apiClient = create({
-    baseURL: "http://192.168.1.4:3000/api",
+    baseURL: "http://192.168.1.11:3000/api"
 });
 
 export default apiClient;
