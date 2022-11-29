8dffe6d882cdd1e8dbc6e3ac8d6ef3e2f5c7acbe
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-11-29 19:25:31 +0530

    Updates on client and dependencies

--- client/resource-tracker/app/apis/client.js
+++ client/resource-tracker/app/apis/client.js
@@ -1,6 +1,6 @@
 import { create } from "apisauce";
 const apiClient = create({
-    baseURL: "http://192.168.1.14:3000/api"
+    baseURL: "http://192.168.140.204:3000/api"
 });
 
 export default apiClient;
