c5439973d6f7988e4884be99563e746f27639804
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2022-11-30 22:33:47 +0530

    Update profile screen

--- client/resource-tracker/app/components/ProfileCard/index.js
+++ client/resource-tracker/app/components/ProfileCard/index.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { colors } from 'react-native-elements'
-import useAuth from '../../auth/useAuth'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { colors } from "react-native-elements";
+import useAuth from "../../auth/useAuth";
 
 export default function ProfileCard() {
-  const { user } = useAuth()
+  const { user } = useAuth();
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -12,49 +12,56 @@ export default function ProfileCard() {
 
   return (
     <View style={styles.container}>
-      <View style={{
-      }}>
-        <View style={styles.imagecontainer}>
-          <Image style={styles.image} source={require("../../../assets/user-icon.png")} />
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.label}> Name:  </Text>
-          <Text style={styles.info}>{user.name}</Text>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.label}> Email:  </Text>
+      <View style={styles.imagecontainer}>
+        <Image
+          style={styles.image}
+          source={require("../../../assets/user-icon.png")}
+        />
+
+        <View style={styles.textContainer}>
+          {/* <Text style={styles.label}> Name: </Text> */}
+          <Text style={{ fontSize: 26, fontWeight: "500" }}>{user.name}</Text>
+
           <Text style={styles.info}>{user.email}</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={styles.label}> Department:  </Text>
-          <Text style={styles.info}> {user.department}</Text>
-        </View>
-        
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.label}> Club:  </Text>
-          {
-            user.role[0] === "representative" && <Text style={styles.info}>{user.representative_club}</Text>}
-          {
-            user.role[0] === "advisor" && <Text style={styles.info}>{user.advisor_clubs}</Text>
-          }
-          
-        </View>
+      </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.label}> Role  </Text>
-          { user.role.map((role, index) => {
-            return <Text style={styles.info}> {capitalizeFirstLetter(role)} </Text>
-          }
-          )}
-        </View>
+      {/* <View style={{ flexDirection: "row" }}>
+          <Text style={styles.label}> Email: </Text>
+          <Text style={styles.info}>{user.email}</Text>
+        </View> */}
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>Department</Text>
+        <Text style={styles.info}>{user.department}</Text>
+      </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.label}> Contact:  </Text>
-          <Text style={styles.info}>{user.mobile_number}</Text>
-        </View>
+      <View style={styles.textContainer}>
+        {user.role[0] === "representative" && (
+          <>
+            <Text style={styles.label}>Club: </Text>
+            <Text style={styles.info}>{user.representative_club}</Text>
+          </>
+        )}
+        {user.role[0] === "advisor" && (
+          <Text style={styles.info}>{user.advisor_clubs}</Text>
+        )}
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>Role </Text>
+        {user.role.map((role, index) => {
+          return (
+            <Text style={styles.info}>{capitalizeFirstLetter(role)} </Text>
+          );
+        })}
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>Contact </Text>
+        <Text style={styles.info}>{user.mobile_number}</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -63,25 +70,29 @@ const styles = StyleSheet.create({
     height: 80,
     borderRadius: 50,
     resizeMode: "contain",
+    marginRight: 20,
   },
   container: {
-    margin: 10,
-    padding: 10,
+    paddingHorizontal: 20,
     borderColor: colors.grey3,
-    borderWidth: 2,
-    borderRadius: 5
   },
   info: {
     fontSize: 18,
   },
   label: {
     fontSize: 18,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
 
   imagecontainer: {
+    flexDirection: "row",
     paddingVertical: 10,
-    marginBottom: 10
-  }
+    marginBottom: 10,
+    // justifyContent: 'space-between',
+  },
 
-})
+  textContainer: {
+    flexDirection: "column",
+    paddingVertical: 5,
+  },
+});
--- client/resource-tracker/app/screens/UserProfile.js
+++ client/resource-tracker/app/screens/UserProfile.js
@@ -1,14 +1,124 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import ProfileCard from '../components/ProfileCard'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+import useAuth from "../auth/useAuth";
+import { colors } from "../configs/variables";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 export default function UserProfile() {
+  const { user } = useAuth();
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
-    <View style={{
-      flex: 1,
-      marginTop: 50
-    }}>
-      <ProfileCard />
+    <View style={styles.container}>
+      <View style={styles.imagecontainer}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/user-icon.png")}
+        />
+
+        <View style={styles.textContainer}>
+          {/* <Text style={styles.label}> Name: </Text> */}
+          <Text style={{ fontSize: 26, fontWeight: "500" }}>{user.name}</Text>
+
+          <Text style={styles.info}>{user.email}</Text>
+        </View>
+      </View>
+
+      {/* <View style={{ flexDirection: "row" }}>
+          <Text style={styles.label}> Email: </Text>
+          <Text style={styles.info}>{user.email}</Text>
+        </View> */}
+      <View style={styles.textContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <FontAwesome5 name="building" size={20} color="black" />
+          <Text style={styles.label}>Department</Text>
+        </View>
+        <Text style={styles.info}>{user.department}</Text>
+      </View>
+
+      
+        {user.role[0] === "representative" && (
+          <View style={styles.textContainer}>
+            <View style={{ flexDirection: "row" }}>
+              <FontAwesome5 name="building" size={20} color="black" />
+              <Text style={styles.label}>Club </Text>
+            </View>
+            <Text style={styles.info}>{user.representative_club}</Text>
+            </View>
+        )}
+
+        {user.role[0] === "advisor" && (
+          
+
+          <View style={styles.textContainer}>
+            <View style={{ flexDirection: "row" }}>
+              <FontAwesome5 name="building" size={20} color="black" />
+              <Text style={styles.label}>Club </Text>
+            </View>
+            <Text style={styles.info}>{user.advisor_clubs}</Text>
+            </View>
+        )}
+     
+
+      <View style={styles.textContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <Feather name="user" size={20} color="black" />
+          <Text style={styles.label}>Role </Text>
+        </View>
+        {user.role.map((role, index) => {
+          return (
+            <Text style={styles.info}>{capitalizeFirstLetter(role)} </Text>
+          );
+        })}
+      </View>
+
+      <View style={styles.textContainer}>
+        <View style={{ flexDirection: "row" }}>
+          <Entypo name="mobile" size={20} color="black" />
+          <Text style={styles.label}>Contact </Text>
+        </View>
+        <Text style={styles.info}>{user.mobile_number}</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    resizeMode: "contain",
+    marginRight: 20,
+  },
+  container: {
+    paddingHorizontal: 20,
+    borderColor: colors.grey3,
+  },
+  info: {
+    fontSize: 18,
+  },
+  label: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  imagecontainer: {
+    flexDirection: "row",
+    paddingVertical: 10,
+    marginBottom: 10,
+    // justifyContent: 'space-between',
+  },
+
+  textContainer: {
+    flexDirection: "column",
+    paddingVertical: 10,
+  },
+});
