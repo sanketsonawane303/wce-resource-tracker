dfc902e5089dc52f370e1835c3a6afa95fe1a494
shaikhamaan <amaan.shaikh@walchandsangli.ac.in>
2022-12-04 16:03:31 +0530

    Update upload and keyUpdate routes

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import getKeys from "../controllers/key/getKeys.js";
-import updateKeyStatus from "../controllers/key/updateKeyStatus.js";
 import uploadId from "../controllers/upload/uploadId.js";
 
 import role from "../middlewares/role.js";
@@ -8,9 +6,8 @@ import { upload } from "../middlewares/upload.js";
 
 const router = Router();
 
-router.get("/", role(["hod", "advisor"]), getKeys);
-router.put(
-  "/upload",
+router.post(
+  "/id",
   role(["helper"]),
   upload.fields([
     { name: "photo", maxCount: 1 },
@@ -18,11 +15,4 @@ router.put(
   ]),
   uploadId
 );
-router.put(
-  "/status",
-  role(["helper"]),
-
-  updateKeyStatus
-);
-
 export default router;
