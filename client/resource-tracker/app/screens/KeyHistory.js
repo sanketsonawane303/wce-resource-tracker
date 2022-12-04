d70630c2232717b9fed33f8a8b45f1bfa5927301
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-12-04 23:50:53 +0530

    Update Home screen

---
+++ client/resource-tracker/app/screens/KeyHistory.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const KeyHistory = () => {
+    return (
+        <View>
+            <Text>KeyHistory</Text>
+        </View>
+    )
+}
+
+export default KeyHistory
+
+const styles = StyleSheet.create({})
